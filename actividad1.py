43aaa79c006227784993fe3f6b8c5360fd27199f
Jonkkeyler333 <82979079+Jonkkeyler333@users.noreply.github.com>
2023-05-14 13:10:18 -0500

    se solucionaron errores con el método add_tail y add_head , olvide el parentesis en la llamada al metodo is.empty

--- actividad1.py
+++ actividad1.py
@@ -1,9 +1,9 @@
 class lista_enlazada:
     class nodo:
-      __slots__='element','siguiente'
-      def __init__(self,elemento,siguiente) -> None:
+      __slots__='element','next'
+      def __init__(self,elemento,next) -> None:
          self.element=elemento
-         self.siguiente=siguiente
+         self.next=next
            
     def __init__(self) -> None:
        self.head=None
@@ -18,19 +18,29 @@ class lista_enlazada:
     
     def add_head(self,elemento)->None:
       new_nodo=lista_enlazada.nodo(elemento,self.head)
-      if self.is_empty:
+      if self.is_empty():
         self.tail=new_nodo
       self.head=new_nodo
       self.size+=1
     
     def add_tail(self,elemento)->None:
       new_nodo=lista_enlazada.nodo(elemento,None)
-      if self.is_empty:
+      if self.is_empty():
         self.head=new_nodo
       else:
-        self.tail.siguiente=new_nodo
+        self.tail.next=new_nodo
       self.tail=new_nodo
       self.size+=1
+      
+    def __str__(self) -> str:
+      current_nodo=self.head
+      cadena=''
+      while current_nodo:
+        cadena+=str(current_nodo.element)
+        if current_nodo.next:
+          cadena+='->'
+        current_nodo=current_nodo.next
+      return cadena
       
 if __name__=='__main__':
   l1=lista_enlazada()
@@ -39,6 +49,10 @@ if __name__=='__main__':
   print(len(l1))
   l1.add_head(23)
   print(len(l1))
+  l1.add_head(3444)
+  l1.add_head(2000)
+  l1.add_tail(21221)
+  print(l1)
   
   
       
