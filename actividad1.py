796dbb1de91c338e883455d75fce509bb1fc520a
Jonkkeyler333 <82979079+Jonkkeyler333@users.noreply.github.com>
2023-05-14 12:20:04 -0500

    elimine un archivo y cree la clase lista_enlazada

--- actividad1.py
+++ actividad1.py
@@ -1,1 +1,19 @@
-print('hello world')
+class lista_enlazada:
+    class nodo:
+      __slots__='element','siguiente'
+      def __init__(self,elemento,siguiente:int) -> None:
+         self.element=elemento
+         self.siguiente=siguiente
+           
+    def __init__(self) -> None:
+       self.head=None
+       self.size=0
+    
+    def __len__(self)->int:
+      return self.size
+    
+    def is_empty(self)->bool:
+      return  self.size==0
+    
+    def add_head(self)->None:
+      pass
